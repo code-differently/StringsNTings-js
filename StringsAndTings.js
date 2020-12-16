2b01e1e613e26fa37fb414961db2b51a857b5e6c
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-16 12:37:16 -0500

    Complete reverseWords()

--- StringsAndTings.js
+++ StringsAndTings.js
@@ -17,9 +17,9 @@ class StringsAndTings {
         // Strings in js are immutable. Need to split it into a new array to modify it.
         let result = str.split("");
         let length = result.length;
-        // You would think you'd need to do -1 here, but that does not work. This method seems to work for even and odd lengthed inputs.
-        const halfIndex = Math.floor(length / 2);
-        for (let i = 0; i < halfIndex; i++) {
+        // You would think you'd need to subtract 1 from here, but that does not work. This method seems to work for even and odd lengthed inputs.
+        const halfLength = Math.floor(length / 2);
+        for (let i = 0; i < halfLength; i++) {
             let rightLetter = result[length - i - 1];
             result[length - i - 1] = result[i];
             result[i] = rightLetter;
@@ -29,7 +29,13 @@ class StringsAndTings {
     }
 
     reverseWords(str) {
-        return null;
+        let words = str.split(" ");
+        let result = "";
+        for (let word of words) {
+            word = this.reverseString(word);
+            result += `${word} `;
+        }
+        return result.trim();
     }
 
     // @return string with identical contents, in reverse order, with first character capitalized
