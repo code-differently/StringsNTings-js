de24eaaba7bda7cac36d06ab8e8ae024dd0878f9
Kalyn Smith <kalyn@Kalyns-MacBook-Air.local>
2020-12-16 21:36:38 -0500

    Completetd StringsAndTings

--- StringsAndTings.js
+++ StringsAndTings.js
@@ -2,28 +2,64 @@ class StringsAndTings {
     
     // @return string with identical content, and the first character capitalized
     camelCase(str){
-        return null;
+        let strSplit= str.split(" ");
+        for(let i = 0; i < strSplit.length; i++){
+            let word = strSplit[i];
+            strSplit[i] = word[0].toUpperCase() + word.substring(1);
+        }
+        return strSplit.join(" ");
     }
 
     //@return string with identical contents, with each word individually in reverse order
     reverseString(str){
-        return null;
+        let strSplit = str.split(" ");
+            for (let i=0; i < strSplit.length; i++) {
+                let word = strSplit[i];
+                strSplit[i] = word[i] + word.substring(1);
+            }
+            return str.split("").reverse().join("");
     }
-
+    
+    
     // @return string with identical contents, in reverse order, with first character capitalized
     reverseThenCamelCase(str){
-        return null;
+        let reverseCamel = this.reverseString(str);
+        return this.camelCase(reverseCamel);      
     }
 
     // @return string with identical contents excluding first and last character
     removeFirstAndLastCharacter(str){
-        return null;
+        let remove = str.substring(1, str.length-1);
+        return remove;
     }
 
     // @return string with identical characters, each with opposite casing
     invertCasing(str){
-        return null;
+        let stringNew = "";
+        let i = 0;
+        while (i < str.length) {
+            let k = str.charAt(i);
+            if (k == k.toUpperCase()) {
+                k = k.toLowerCase();
+            } else {
+                k = k.toUpperCase();
+            }
+            i += 1;
+            stringNew += k; 
+        }
+        return stringNew;
+        }
+
+       // reverse words
+       reverseWords(str){
+        let words = str.split(" ");
+        let result ="";
+            for (let i = 0; i < words.length; i++) {
+               result += words[i].split('').reverse().join('') + " ";
+        }
+            return result.trim();
     }
+
 }
 
 module.exports = StringsAndTings;
