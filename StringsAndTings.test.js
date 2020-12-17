b10e773fb1e3f394dd74f66f0baccdc0f8207c22
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-12-17 09:49:55 -0500

    all but camel casing

--- StringsAndTings.test.js
+++ StringsAndTings.test.js
@@ -8,7 +8,7 @@ test("camelCaseTest", () => {
     let input = "she sells sea shells";
     let expected = "She Sells Sea Shells";
 
-    let actual = stringsAndTings.camelCase(15);
+    let actual = stringsAndTings.camelCase(input);
 
     expect(actual).toEqual(expected);
 });
@@ -19,7 +19,7 @@ test("reverseTest", () => {
     let input = "she sells sea shells";
     let expected = "sllehs aes slles ehs";
 
-    let actual = stringsAndTings.camelCase(15);
+    let actual = stringsAndTings.reverseTest(input);
 
     expect(actual).toEqual(expected);
 });
@@ -30,7 +30,7 @@ test("reverseWordsTest", () => {
     let input = "she sells sea shells";
     let expected = "ehs slles aes sllehs";
 
-    let actual = stringsAndTings.camelCase(15);
+    let actual = stringsAndTings.reverseWordsTest(input);
 
     expect(actual).toEqual(expected);
 });
@@ -41,7 +41,7 @@ test("reverseThenCamelCaseTest", () => {
     let input = "she sells sea shells";
     let expected = "Sllehs Aes Slles Ehs";
 
-    let actual = stringsAndTings.camelCase(15);
+    let actual = stringsAndTings.reverseThenCamelCaseTest(input);
 
     expect(actual).toEqual(expected);
 });
@@ -52,19 +52,19 @@ test("removeFirstAndLastCharacterTest", () => {
     let input = "she sells sea shells";
     let expected = "he sells sea shell";
 
-    let actual = stringsAndTings.camelCase(15);
+    let actual = stringsAndTings.firstAndLastCharacterTest(input);
 
     expect(actual).toEqual(expected);
 });
 
 
-test("invertCasingTest1", () => {
+test("invertCasing", () => {
 
     let stringsAndTings = new StringsAndTings();
     let input = "shE sells SEA sHeLlS";
     let expected = "SHe SELLS sea ShElLs";
 
-    let actual = stringsAndTings.camelCase(15);
+    let actual = stringsAndTings.invertCasing(input);
 
     expect(actual).toEqual(expected);
 });
